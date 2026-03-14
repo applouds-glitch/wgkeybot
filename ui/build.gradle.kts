8d8112b12493edf8a56dbf4086423f6ee58f666f
kiper292 <kiper292@gmail.com>
2026-03-14 18:29:58 +0500

    try to fix apk signing

--- ui/build.gradle.kts
+++ ui/build.gradle.kts
@@ -44,6 +44,7 @@ android {
         release {
             isMinifyEnabled = true
             isShrinkResources = true
+            signingConfig = signingConfigs.getByName("debug")
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-android-optimize.txt")
             packaging {
                 resources {
